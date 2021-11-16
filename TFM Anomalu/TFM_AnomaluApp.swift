cee62c3740a18896f05f685947555fb3479e00a0
Harry Lopez <jlopezv@fundaciongruposocial.co>
2021-11-16 18:15:26 -0500

    Initial Commit

---
+++ TFM Anomalu/TFM_AnomaluApp.swift
@@ -0,0 +1,17 @@
+//
+//  TFM_AnomaluApp.swift
+//  TFM Anomalu
+//
+//  Created by BCS on 16/11/21.
+//
+
+import SwiftUI
+
+@main
+struct TFM_AnomaluApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
